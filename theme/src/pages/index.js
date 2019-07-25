b505aff42bcaf3f52f30e43f4b992a9598d2e067
Andi Hakim <andi.hakim.arif@gmail.com>
2019-07-25 22:09:59 +0700

    add dep

---
+++ theme/src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const Styled = styled.div`
+  color: tomato;
+`;
+
+const Index = () => <Styled>Boo</Styled>;
+export default Index;
