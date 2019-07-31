8aa449529d00c8ba9f9b3485eebedd520b69571e
Andi Hakim <andi.hakim.arif@gmail.com>
2019-07-31 07:16:36 +0700

    layout for detail job

--- theme/src/components/posts-page-layout.js
+++ theme/src/components/posts-page-layout.js
@@ -5,9 +5,39 @@ import NavBar from "./ui/NavBar";
 import RemoveMargin from "./RemoveMargin";
 import styled from "styled-components";
 
+import logo from "../icons/function.svg";
+import location from "../icons/location.svg";
+
 const Layout = styled.div`
   max-width: 800px;
-  margin: 0px auto 50px auto;
+  margin: 30px auto 50px auto;
+  display: flex;
+  line-height: 1.75rem;
+`;
+
+const Left = styled.div`
+  background-color: tomato;
+  min-width: 550px;
+  padding-left: 20px;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+`;
+
+const Right = styled.div`
+  background-color: aliceblue;
+  min-width: 250px;
+  padding-left: 30px;
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+  line-height: 3rem;
+`;
+
+const ImgFunction = styled.img`
+  margin-right: 3px;
+`;
+const ImgLocation = styled.img`
+  margin-left: 4px;
+  margin-right: 6px;
 `;
 
 export default function PageTemplate({ data: { mdx } }) {
@@ -18,9 +48,34 @@ export default function PageTemplate({ data: { mdx } }) {
       <NavBar />
       <RemoveMargin />
       <Layout>
-        <h1>{mdx.frontmatter.title}</h1>
-        {/* <MDXRenderer>{mdx.rawBody}</MDXRenderer> */}
-        <MDXRenderer>{mdx.body}</MDXRenderer>
+        <Left>
+          <MDXRenderer>{mdx.body}</MDXRenderer>
+        </Left>
+        <Right>
+          <h1>{mdx.frontmatter.title}</h1>
+          <div>
+            <ImgFunction src={logo} alt="Logo" />
+
+            {mdx.frontmatter.function}
+          </div>
+          <div>
+            <ImgLocation src={location} alt="Logo" />
+            {mdx.frontmatter.location}
+          </div>
+          <div>
+            <a
+              href={
+                "mailto:" +
+                mdx.frontmatter.apply +
+                "?subject=" +
+                mdx.frontmatter.title
+              }
+              type="button"
+            >
+              <button type="submit">Apply</button>
+            </a>
+          </div>
+        </Right>
       </Layout>
     </div>
   );
@@ -32,11 +87,11 @@ export const pageQuery = graphql`
       id
       frontmatter {
         title
+        function
+        location
+        apply
       }
-      # code {
       body
-      # }
-      # rawBody
     }
   }
 `;
